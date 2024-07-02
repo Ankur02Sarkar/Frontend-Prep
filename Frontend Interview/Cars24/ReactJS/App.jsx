1dde8394977c56effdee477154210ab22b1edcf5
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-07-02 19:44:18 +0530

    removed all

--- Frontend Interview/Cars24/ReactJS/App.jsx
+++
@@ -1,6 +0,0 @@
-// Implement File Explorer using a JSON Object
-// Already done in "../../../Frontend Machine Coding/React JS/FileExplorer/viewOnly/App"
-
-// Follow Up Question :-
-// What type of Props should a reusable component like this should accept to make it Generic or Open Source?
-// Answer :- OnSelect, OnExpand, OnClose
